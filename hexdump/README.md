503f637228daf34e7a405d14da14595199852ff6
Matthew Kerwin <matthew@kerwin.net.au>
2013-03-01 17:59:36 +1000

    added hexdump experiment

---
+++ hexdump/README.md
@@ -0,0 +1,8 @@
+Hexdump
+=======
+
+A very, very old library I wrote back in 2009 to emulate
+the behaviour of the Linux `hexdump` command.
+
+Handy for debugging binary data.
+
---
+++ hexdump/demo.rb
@@ -0,0 +1,16 @@
+# vim:ts=2:sw=2:sts=2
+
+if Kernel.method_defined? :require_relative
+		require_relative 'hexdump.rb'
+else
+		require './hexdump.rb'
+end
+
+str = "lkjasdlkjasd\nalsdhlkjhaskdjh\n\nlkjhasdkjhaksjhdas\e[1mANSI MAGIC\e[0mklhkjhlysdufkhjknasdfkhjl7yk\nuhdfiuhdsf\t\tlkjhdsf"
+#puts "\nThe String:", str
+puts "\ninspect:",   str.inspect
+puts "\nprintable:", str.printable
+puts "\nwrap_hex:",            str.wrap_hex,      '', str.wrap_hex(true)
+puts "\nwrap_printable:",      str.wrap_printable,'', str.wrap_printable(true)
+puts "\nhexdump:",             str.hexdump,       '', str.hexdump(true)
+
