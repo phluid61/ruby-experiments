d65d566e7c1ac65782b20468ef17cc1c5b7c855b
Matthew Kerwin <matthew@kerwin.net.au>
2013-04-05 09:56:37 +1000

    more awesomeness

--- send_if/send_if.rb
+++ send_if/send_if.rb
@@ -4,11 +4,16 @@ class Object
 		respond_to?(meth) ? send(meth, *args) : nil
 	end
 
-	def and_send meth, *args
+	def send_if_not_nil meth, *args
+		send meth, *args
+	end
+
+	def send_if_true meth, *args
 		send meth, *args
 	end
 end
 
-def nil.and_send *args; self; end
-def false.and_send *args; self; end
+def nil.send_if_not_nil *args; self; end
+def nil.send_if_true *args; self; end
+def false.send_if_true *args; self; end
 
--- send_if/test.rb
+++ send_if/test.rb
@@ -1,18 +1,29 @@
 require 'test/unit'
 $VERBOSE = true
 
+$a = {'a'=>{'a'=>{'a'=>'a'}}}
+$b = {'a'=>{}}
+$c = {'a'=>{'a'=>false}}
+
 require './send_if'
 class Test_send_if < Test::Unit::TestCase
 	def test_send_if_respond_to
 		assert_equal( "ABC", 'abc'.send_if_respond_to(:upcase) )
 		assert_nil(          12345.send_if_respond_to(:upcase) )
 	end
-	def test_and_send
-		a = {'a'=>{'a'=>{'a'=>'a'}}}
-		b = {'a'=>{}}
+	def test_send_if_not_nil
+		trial = lambda{|h| h.send_if_not_nil(:[],'a').send_if_not_nil(:[],'a').send_if_not_nil(:[],'a') }
 
-		assert_equal( "a", a.and_send(:[],'a').and_send(:[],'a').and_send(:[],'a') )
-		assert_nil(        b.and_send(:[],'a').and_send(:[],'a').and_send(:[],'a') )
+		assert_equal( "a", trial[$a] )
+		assert_nil(        trial[$b] )
+		assert_raise(NoMethodError) { trial[$c] }
+	end
+	def test_send_if_true
+		trial = lambda{|h| h.send_if_true(:[],'a').send_if_true(:[],'a').send_if_true(:[],'a') }
+
+		assert_equal( "a", trial[$a] )
+		assert_nil(        trial[$b] )
+		assert_equal(false,trial[$c] )
 	end
 end
 
