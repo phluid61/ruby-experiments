0fa56caa5a3534063e6c55db63643821abd365a5
Matthew Kerwin <matthew@kerwin.net.au>
2013-04-10 12:40:06 +1000

    added andand pattern

---
+++ send_if/andand.rb
@@ -0,0 +1,28 @@
+
+class AndAndDelegator
+	def initialize o
+		@o = o
+		@chain = []
+	end
+	def _?
+		self
+	end
+	def _!
+		@chain.inject(@o) do |o,x|
+			a,b = x
+			break o unless o
+			o.__send__(*a, &b)
+		end
+	end
+	def method_missing *a, &b
+		@chain << [a,b]
+		self
+	end
+end
+
+class Object
+	def _?
+		AndAndDelegator.new(self)
+	end
+end
+
