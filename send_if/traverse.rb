29e1bacc544edf804e22cd674c817825f5604de2
Matthew Kerwin <matthew@kerwin.net.au>
2013-04-11 17:25:20 +1000

    better chicken-typing
    Now does a much better job of checking that an object can respond to #fetch
    Also added some hackery to Thread, just 'cause.

--- send_if/traverse.rb
+++ send_if/traverse.rb
@@ -2,8 +2,8 @@
 module Traversal
 	def traverse *path, &block
 		obj = self
-		while !path.empty? and obj.respond_to? :fetch
-			obj = obj.fetch(path.shift) do |k|
+		while !path.empty? and (fobj = Traversal.fetchable(obj))
+			obj = fobj.fetch(path.shift) do |k|
 				return yield obj, k if block_given?
 				return Traversal.default(obj, k)
 			end
@@ -13,6 +13,16 @@ module Traversal
 			return nil
 		end
 		obj
+	end
+
+	def Traversal.fetchable obj
+		if obj.respond_to? :fetch
+			obj
+		elsif obj.respond_to? :to_h
+			obj.to_h
+		elsif obj.respond_to? :to_a
+			obj.to_a
+		end
 	end
 
 	def Traversal.default obj, k
@@ -32,15 +42,10 @@ class Array
 	include Traversal
 end
 
-class Struct
-	def fetch *a, &b
-		to_h.fetch(*a, &b)
+# For the lawls
+class Thread
+	def to_h
+		keys.inject({}) {|h,k| h[k] = self[k] }
 	end
-	include Traversal
-end
-
-# Because
-class << ENV
-	extend Traversal
 end
 
