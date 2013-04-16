87111a08028488d0fdcc2d4d351f7153f34897b9
Matthew Kerwin <matthew@kerwin.net.au>
2013-04-16 20:53:54 +1000

    better coverage, from maybe test

--- send_if/test/andand.rb
+++ send_if/test/andand.rb
@@ -15,7 +15,7 @@ class C
   def to_i; 1; end
 end
 
-def nil.c
+def false.c
 	2
 end
 
@@ -24,22 +24,36 @@ class Test_andand < Test::Unit::TestCase
 	def test_andand_nil
 		a = A.new
 		assert_equal( 1, a._?.b.c.to_i._! )
+		assert_equal( 1, a._?.b.c._!.to_i )
+		assert_equal( 1, a._?.b._!.c.to_i )
 		a.b.c = nil
 		assert_nil( a._?.b.c.to_i._! )
+		assert_equal( 0, a._?.b.c._!.to_i )
+		assert_equal( 0, a._?.b._!.c.to_i )
 		a.b = nil
 		assert_nil( a._?.b.c.to_i._! )
+		assert_equal( 0, a._?.b.c._!.to_i )
+		assert_raise(NoMethodError) { a._?.b._!.c.to_i }
 		a = nil
 		assert_nil( a._?.b.c.to_i._! )
 	end
 	def test_andand_false
 		a = A.new
 		assert_equal( 1, a._?.b.c.to_i._! )
+		assert_equal( 1, a._?.b.c._!.to_i )
+		assert_equal( 1, a._?.b._!.c.to_i )
 		a.b.c = false
 		assert_equal( false, a._?.b.c.to_i._! )
+		assert_raise(NoMethodError) { a._?.b.c._!.to_i }
+		assert_raise(NoMethodError) { a._?.b._!.c.to_i }
 		a.b = false
 		assert_equal( false, a._?.b.c.to_i._! )
+		assert_raise(NoMethodError) { a._?.b.c._!.to_i }
+		assert_equal( 2, a._?.b._!.c.to_i )
 		a = false
 		assert_equal( false, a._?.b.c.to_i._! )
+		assert_raise(NoMethodError) { a._?.b.c._!.to_i }
+		assert_equal( 2, a._?.b._!.c.to_i )
 	end
 end
 
