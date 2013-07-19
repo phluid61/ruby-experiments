6993855244874200189be7b3d59d50f3fcc51f87
Matthew Kerwin <matthew@kerwin.net.au>
2013-07-20 00:41:20 +1000

    returning a scope/block is bad

--- gettime/ext/gettime/gettime.c
+++ gettime/ext/gettime/gettime.c
@@ -29,18 +29,15 @@ static VALUE v_CLOCK_REALTIME, v_CLOCK_MONOTONIC,
 
 #ifdef HAVE_CLOCK_GETTIME
 
-#  ifdef HAVE_LONG_LONG
-#    define timespec2num(ts) { \
-      if (!MUL_OVERFLOW_SIGNED_INTEGER_P(1000000000, (LONG_LONG)ts.tv_sec, LLONG_MIN, LLONG_MAX-999999999)) { \
-	return LL2NUM(ts.tv_nsec + 1000000000 * (LONG_LONG)ts.tv_sec); \
-      } \
-      return rb_funcall(LONG2FIX((ts).tv_nsec), '+', 1, rb_funcall(LONG2FIX(1000000000), '*', 1, UINT2NUM((ts).tv_sec))); \
+static inline VALUE
+timespec2num(ts) {
+#ifdef HAVE_LONG_LONG
+    if (!MUL_OVERFLOW_SIGNED_INTEGER_P(1000000000, (LONG_LONG)ts.tv_sec, LLONG_MIN, LLONG_MAX-999999999)) {
+	return LL2NUM(ts.tv_nsec + 1000000000 * (LONG_LONG)ts.tv_sec);
     }
-#  else
-#    define timespec2num(ts) { \
-      return rb_funcall(LONG2FIX((ts).tv_nsec), '+', 1, rb_funcall(LONG2FIX(1000000000), '*', 1, UINT2NUM((ts).tv_sec))); \
-    }
-#  endif
+#endif
+    return rb_funcall(LONG2FIX((ts).tv_nsec), '+', 1, rb_funcall(LONG2FIX(1000000000), '*', 1, UINT2NUM((ts).tv_sec)));
+}
 /*
 #    define timespec2num(ts) UINT2NUM((ts).tv_sec*1000000000 + (ts).tv_nsec)
 */
