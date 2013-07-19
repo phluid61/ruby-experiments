0a2115946f209c9847ed3e835bd1df7bc238d794
Matthew Kerwin <matthew@kerwin.net.au>
2013-07-20 00:07:07 +1000

    each -> each_pair

--- gettime/test.rb
+++ gettime/test.rb
@@ -1,6 +1,5 @@
 require_relative 'lib/gettime'
 
-Process.clocks.each do |name,id|
+Process.clocks.each_pair do |name,id|
 	puts "#{name}: #{Process.clock_gettime(id)} @ #{Process.clock_getres(id)}"
 end
-
