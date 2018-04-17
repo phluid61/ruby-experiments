b591bd90e75754380e52f9a6c4eb29ee981df365
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2018-04-17 14:37:03 +1000

    use a boring superclass
    replace all the interesting, efficient arithmetic with some dodgy string
    manipulations

--- base32/base32/crockford-base32.rb
+++ base32/base32/crockford-base32.rb
@@ -1,65 +1,40 @@
+require_relative 'core'
+
 # http://www.crockford.com/wrmg/base32.html
 module CrockfordBase32
   class <<self
+    include Base32Core
+
     DICT = %w[0 1 2 3 4 5 6 7 8 9 A B C D E F G H J K M N P Q R S T V W X Y Z]
     RDICT = Hash[DICT.each_with_index.to_a]
 
-    CHECK = %w[* ~ $ = U]
+    CHECK = DICT + %w[* ~ $ = U]
     RCHECK = Hash[CHECK.each_with_index.to_a]
 
-    def decode32 str
+    def decode32 str, checksum=false
       # ignore padding, invalid chars
-      out = +''.b
-      return out if str.empty?
-      return out if str == '*'
+      return '' if str.empty?
+      return '' if str == '0'
       str = str.b.upcase.gsub('-','').gsub('O','0').gsub(/[IL]/,'1')
-      str = str.gsub(/[^0-9A-HJKMNP-TV-Z]+[*~$=U]?/, '')
-
-      if str.sub!(/([*~$=U])\Z/, '')
-        checksum = RCHECK[$1]
-      else
-        checksum = nil
-      end
-
-      str.scan(/.{1,8}/).each do |chunk|
-        if chunk.length == 8
-          bits = chunk.each_char.map{|chr| RDICT[chr] }.reduce(0){|accum, e| (accum << 5) | e }
-          out << [bits].pack('Q>')[-5..-1]
-        else
-          bit_pool = 0
-          pool_size = 0
-
-          chunk.each_char do |chr|
-            pentet = RDICT[chr]
-
-            bit_pool = (bit_pool << 5) | pentet
-            pool_size += 5
-          end
-
-          remainder = pool_size % 8
-          #-- validate padding
-          mask = (1 << remainder) - 1
-          warn 'detected non-zero padding bits in base32 data' unless (bit_pool & mask).zero?
-          #--
-          bit_pool >>= remainder
-
-          num_bytes = pool_size / 8
-          bytes = [0] * num_bytes
-          num_bytes.times do |i|
-            bytes[num_bytes - i - 1] = (bit_pool & 0xFF).chr
-            bit_pool >>= 0
-          end
-
-          out << bytes.join
+      if checksum == :detect
+        if str.sub!(/([*~$=U])\Z/, '')
+          checksum = RCHECK[$1]
         end
+      elsif checksum
+        str.sub!(/(.)\Z/, '')
+        checksum = RCHECK[$1]
       end
+      str = str.gsub(/[^0-9A-HJKMNP-TV-Z]+/, '')
+
+      bin, pad = _decode32(str)
+      warn 'detected non-zero padding bits' unless pad =~ /\A0*\z/
 
       if checksum
-        checkvalue = out.each_byte.reduce(0){|sum, byte| (sum + byte) % 37 }
+        checkvalue = bin.each_byte.reduce(0){|sum, byte| (sum + byte) % 37 }
         raise ArgumentError, "bad checksum in crockford-base32 data (got #{checkvalue}, expected #{checksum})" if checkvalue != checksum
       end
 
-      out
+      bin
     end
 
     def encode32 bin, checksum=false
@@ -68,95 +43,40 @@ module CrockfordBase32
       strict.scan(/.{1,8}/).join('-')
     end
 
-    def strict_decode32 str
-      out = +''.b
-      return out if str.empty?
-      return out if str == '*'
+    def strict_decode32 str, checksum=false
+      return '' if str.empty?
+      return '' if str == '0'
       str = str.b.upcase.gsub('-','').gsub('O','0').gsub(/[IL]/,'1')
-      str = str.gsub(/[^0-9A-HJKMNP-TV-Z]+[*~$=U]?/, '')
-      raise ArgumentError, 'invalid crockford-base32' unless str =~ /\A[0-9A-HJKMNP-TV-Z]+[*~$=U]?\z/
-
-      if str.sub!(/([*~$=U])\Z/, '')
-        checksum = RCHECK[$1]
-      else
-        checksum = nil
-      end
-
-      str.scan(/.{1,8}/).each do |chunk|
-        if chunk.length == 8
-          bits = chunk.each_char.map{|chr| RDICT[chr] or raise ArgumentError, 'invalid crockford-base32' }.reduce(0){|accum, e| (accum << 5) | e }
-          out << [bits].pack('Q>')[-5..-1]
-        else
-          bit_pool = 0
-          pool_size = 0
-
-          chunk.each_char do |chr|
-            pentet = RDICT[chr]
-
-            bit_pool = (bit_pool << 5) | pentet
-            pool_size += 5
-          end
-
-          remainder = pool_size % 8
-          #-- validate padding
-          mask = (1 << remainder) - 1
-          raise ArgumentError, 'invalid crockford-base32 (non-zero padding bits)' unless (bit_pool & mask).zero?
-          #--
-          bit_pool >>= remainder
-
-          num_bytes = pool_size / 8
-          bytes = [0] * num_bytes
-          num_bytes.times do |i|
-            bytes[num_bytes - i - 1] = (bit_pool & 0xFF).chr
-            bit_pool >>= 0
-          end
-
-          out << bytes.join
+      if checksum == :detect
+        if str.sub!(/([*~$=U])\Z/, '')
+          checksum = RCHECK[$1]
         end
+      elsif checksum
+        str.sub!(/(.)\Z/, '')
+        checksum = RCHECK[$1]
       end
+      raise ArgumentError, 'invalid crockford-base32' unless str =~ /\A[0-9A-HJKMNP-TV-Z]+\z/
+
+      bin, pad = _decode32(str)
+      raise ArgumentError, 'invalid crockford-base32 (non-zero padding bits)' unless pad =~ /\A0*\z/
 
       if checksum
-        checkvalue = out.each_byte.reduce(0){|sum, byte| (sum + byte) % 37 }
+        checkvalue = bin.each_byte.reduce(0){|sum, byte| (sum + byte) % 37 }
         raise ArgumentError, "bad checksum in crockford-base32 data (got #{checkvalue}, expected #{checksum})" if checkvalue != checksum
       end
 
-      out
+      bin
     end
 
     def strict_encode32 bin, checksum=false
-      arr = []
-
-      bit_pool = 0
-      pool_size = 0
-
-      bytes = bin.bytes
-      loop do
-        while pool_size >= 5
-          shift = pool_size - 5
-          pentet = bit_pool >> shift
-          arr << DICT[pentet]
-
-          bit_pool &= ((1 << shift) - 1)
-          pool_size = shift
-        end
-        if bytes.empty?
-          break if pool_size == 0
-
-          shift = 5 - pool_size
-          pool_size = 5
-          bit_pool <<= shift
-        else
-          bit_pool = (bit_pool << 8) | bytes.shift
-          pool_size += 8
-        end
-      end
+      str = _encode32(bin)
 
       if checksum
-        checkvalue = bin.bytes.reduce(0){|sum, byte| (sum + byte) % 37 }
-        arr << CHECK[checkvalue]
+        checkvalue = bin.each_byte.reduce(0){|sum, byte| (sum + byte) % 37 }
+        str += CHECK[checkvalue]
       end
 
-      arr.join
+      str
     end
   end
 end
