7dfed6a2e8eb7a49b90f0ccdd3eebc1d6e74e025
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2018-08-30 14:05:39 +1000

    add day names, DRY calendary logic

--- packing_formats/ktime.rb
+++ packing_formats/ktime.rb
@@ -9,6 +9,9 @@ class KTime
   MONTH_NAMES = %w{ ProphetsDay Antepod Macropod Phennel Mujuss Rastor Gimbol Fredegar Whipple Spink Jambo Wombley Flank Runcible }
   MONTH_NAMES_ABBR = %w{ XXX Ant Mac Phe Muk Ras Gim Fre Whi Spi Jam Wom Fla Run }
 
+  DAY_NAMES = %w{ BeforeDay ProphetsDay Wunday Zweiday Triday Quidday Fivaday Shipsday Dinky }
+  DAY_NAMES_ABBR = %w{ Bdy Pdy Wun Zwe Tri Qui Fiv Shi Din }
+
   def KTime.now
     KTime.new
   end
@@ -53,66 +56,43 @@ class KTime
 
     leap_year = (y % 4 == 0) and ((y % 100 > 0) or (y % 400 == 0))
 
-    if leap_year
-      if (m < 10) || (m == 10 && d < 23)
-        # it is before DOT, so last year
-        # 1980 => -1
-        # 1981 => 0
-        @year = y - 1981
-        @doy = yday + GREGORIAN_DAYS_AFTER_EPOCH
-        @month = (@doy - 1) / 28 + 1
-        @day = (@doy - 1) % 28 + 1
-      elsif m == 10 && (d == 23 || d == 24)
-        # the blessed day
-        # 1980 => 0
-        # 1981 => 1
-        @year = y - 1980
-        @doy = 0
-        @month = 0
-        @day = d - 23
-      else
-        # it is after DOT, so this year
-        # 1980 => 0
-        # 1981 => 1
-        @year = y - 1980
-        @doy = yday - EPOCH_DAY_OF_GREGORIAN_LEAP_YEAR
-        @month = (@doy - 1) / 28 + 1
-        @day = (@doy - 1) % 28 + 1
-      end
+    if m == 10 && (d == 24 || (d == 23 && leap_year))
+      # the blessed day
+      # 1980 => 0
+      # 1981 => 1
+      @year = y - 1980
+      @doy = 0
+      @month = 0
+      @day = d - 24
+    elsif (m < 10) || (m == 10 && d < 24)
+      # it is before DOT, so last year
+      # 1980 => -1
+      # 1981 => 0
+      @year = y - 1981
+      @doy = yday + GREGORIAN_DAYS_AFTER_EPOCH
+      @month = (@doy - 1) / 28 + 1
+      @day = (@doy - 1) % 28 + 1
     else
-      if (m < 10) || (m == 10 && d < 24)
-        # it is before DOT, so last year
-        # 1980 => -1
-        # 1981 => 0
-        @year = y - 1981
-        @doy = yday + GREGORIAN_DAYS_AFTER_EPOCH
-        @month = (@doy - 1) / 28 + 1
-        @day = (@doy - 1) % 28 + 1
-      elsif m == 10 && d == 24
-        # the blessed day
-        # 1980 => 0
-        # 1981 => 1
-        @year = y - 1980
-        @doy = 0
-        @month = 0
-        @day = 1
-      else
-        # it is after DOT, so this year
-        # 1980 => 0
-        # 1981 => 1
-        @year = y - 1980
-        @doy = yday - EPOCH_DAY_OF_GREGORIAN_YEAR
-        @month = (@doy - 1) / 28 + 1
-        @day = (@doy - 1) % 28 + 1
-      end
+      # it is after DOT, so this year
+      # 1980 => 0
+      # 1981 => 1
+      @year = y - 1980
+      @doy = yday - (leap_year ? EPOCH_DAY_OF_GREGORIAN_LEAP_YEAR : EPOCH_DAY_OF_GREGORIAN_YEAR)
+      @month = (@doy - 1) / 28 + 1
+      @day = (@doy - 1) % 28 + 1
     end
+
+    @dow = (@day <= 0) ? @day : ((@day - 1) % 7 + 1)
   end
 
   def to_s
-    '%3s %3s %2d %X%X%X%X %+05d %04d' % ['???', MONTH_NAMES_ABBR[@month], @day, @match, @egg, @ad, @tic, 0, @year]
+    '%3s %3s %2d %X%X%X%X %+05d %04d' % [DAY_NAMES_ABBR[@dow + 1], MONTH_NAMES_ABBR[@month], @day, @match, @egg, @ad, @tic, 0, @year]
   end
 
-  attr_reader :doy
+  attr_reader :year, :month, :day
+  attr_reader :doy, :dow
+  attr_reader :match, :egg, :ad, :tic, :utic
+  attr_reader :match_of_day, :egg_of_day, :ad_of_day, :tic_of_day
 end
 
 if $0 == __FILE__
